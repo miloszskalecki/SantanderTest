7bb8961ffb9e4b7ea96e8ec62135e5f9078fbea5
Milosz Skalecki <Milosz.Skalecki@GAINCapital.com>
2025-02-03 21:49:35 +0100

    Hacker News Api Client

---
+++ SantanderTest/Clients/HackerNewsStory.cs
@@ -0,0 +1,13 @@
+﻿namespace SantanderTest.Clients;
+
+sealed class HackerNewsStory
+{
+    public long Id { get; init; }
+    public string Title { get; init; } = string.Empty;
+    public string Url { get; init; } = string.Empty;
+    public string By { get; init; } = string.Empty;
+    public bool Dead { get; init; }
+    public long Time { get; init; }
+    public int Score { get; init; }
+    public int Descendants { get; init; }
+}
---
+++ SantanderTest/Clients/IHackerNewsApi.cs
@@ -0,0 +1,8 @@
+﻿namespace SantanderTest.Clients;
+
+interface IHackerNewsApi
+{
+    Task<List<long>?> GetBestStoriesAsync();
+
+    Task<HackerNewsStory?> GetStoryAsync(long id);
+}
